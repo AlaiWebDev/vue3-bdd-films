62be375c3f0df6b5e2433b5376f82c36bb1fe00b
Alain Orluk <alainwebdev@free.fr>
2022-04-07 05:19:25 +0200

    feature-amelioration

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="movie-details">
-      <h2>{{ movie.title }}</h2>
-      <p>Date de sortie : {{ movie.release_date }}</p>
+      <h2 id="titre">{{ movie.title }}</h2>
+      <div class="genres" ><span v-for="genre in genresFilm" :key="genre.name">{{ genre }}&nbsp;</span></div>
+      <h2 id="date_sortie">Date de sortie : <span>{{ dateSortie }}</span></h2>
       <img :src="afficheUrl + movie.backdrop_path" alt="Affiche du film" class="featured-img" />
       <div class="synoptique">
           {{ movie.overview }}
@@ -19,21 +20,32 @@ export default {
     setup () {
         const movie = ref({});
         const route = useRoute();
-        const afficheUrl = env.imgMovieLinks
-        // const affiche = ref("");
-
+        const afficheUrl = env.imgMovieLinks;
+        const genresFilm = [];
+        const dateSortie = ref("");
         onBeforeMount(() => {
             fetch(`https://api.themoviedb.org/3/movie/${route.params.id}?api_key=${env.apikey}&language=fr-FR`)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 movie.value = data;
-            })
+                for (const genre of movie.value.genres) {
+                    genresFilm.push(genre.name);
+                }
+                console.log("L'objet GenresFilm contient : " + genresFilm);
+                var timestamp = Date.parse(movie.value.release_date);
+                var dateTime = new Date(timestamp);
+                console.log("Jour : " + dateTime.getDate());
+                console.log("Mois : " + dateTime.getMonth());
+                dateSortie.value = ((dateTime.getDate().toString().length === 1 ? ("0" +(dateTime.getDate())) : dateTime.getDate())) + "/" + ((((dateTime.getMonth()+1).toString().length === 1) ? ("0" + (dateTime.getMonth()+1)) : dateTime.getMonth()+1)) + "/" + dateTime.getFullYear();
+                console.log("Date Sortie : ", dateSortie);
+            });
         });
 
         return {
             movie,
-            afficheUrl
+            afficheUrl,
+            genresFilm,
+            dateSortie
         }
     }
 }
@@ -43,10 +55,38 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Rowdies:wght@300&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap');
     .movie-details {
-        h2, p, .synoptique {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            p, .synoptique {
             padding: 1rem .5rem;
             font-family: 'Rowdies', cursive;
         }
+
+        #titre {
+            font-size: 1.5rem;
+            padding: 0 .5rem
+        }
+
+        span {
+            font-weight: 200;
+        }
+
+        #genres, #date_sortie {
+            padding: .5rem .5rem
+        }
+
+        .genres {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            align-items: center;
+            padding: .5rem 1rem;
+            bottom: 1rem;
+            left: 0;
+            text-transform: capitalize;
+            }
 
         .synoptique {
             font-family: 'Rowdies', cursive;
@@ -54,6 +94,7 @@ export default {
         img {
             display: block;
             width: 100%;
+            padding: .5rem;
             object-fit: scale-down;
         }
     }
